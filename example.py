8075827288717a29728ff64069380a6e23e788d5
Ali Hakim Taşkıran <52009742+alihakimtaskiran@users.noreply.github.com>
2022-06-05 13:14:48 +0300

    Update example.py

--- example.py
+++ example.py
@@ -1,12 +1,12 @@
 from rainbow import *
 
 DBR=Stack()
-DBR.add_ambient(Ambient([1,]*10, [1,]*10, 'Air'))
-DBR.add_substrate(Substrate([20.25]*10, [1,]*10, 'Silicone-Wafer'))
+DBR.add_ambient(Ambient([1,], [1,], 'Air'))
+DBR.add_substrate(Substrate([20.25], [1,], 'Silicone-Wafer'))
 
 for i in range(8):
-    DBR.add_layer([ThinLayer(123e-9, [4.0779]*10, [1]*10, name=f'Si3N4 {i}'), ThinLayer(151e-9, [2.1025]*10, [1]*10, name=f'SiO2 {i}') ])
+    DBR.add_layer([ThinLayer(123e-9, [4.0779], [1], name=f'Si3N4 {i}'), ThinLayer(151e-9, [2.1025], [1], name=f'SiO2 {i}') ])
 for i in range(8):
-    DBR.add_layer([ThinLayer(151e-9, [2.1025]*10, [1]*10, name=f'SiO2 {8+i}'), ThinLayer(123e-9, [4.0779]*10, [1]*10, name='Si3N4 {i+8}')])
-DBR.Radiation(885e-9, np.deg2rad(i))
+    DBR.add_layer([ThinLayer(151e-9, [2.1025], [1], name=f'SiO2 {8+i}'), ThinLayer(123e-9, [4.0779], [1], name='Si3N4 {i+8}')])
+DBR.Radiation(885e-9)
 DBR.render()
