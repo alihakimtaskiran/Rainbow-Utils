848ce3b5ee78f3868ddab890834b94abc14ce74c
Ali Hakim Taşkıran <52009742+alihakimtaskiran@users.noreply.github.com>
2022-06-12 17:28:29 +0300

    Update example.py

--- example.py
+++ example.py
@@ -1,11 +1,10 @@
-from rainbow import *
-
 DBR=Stack()
-DBR.add_ambient(Ambient([1,], [1,], 'Air'))
-DBR.add_substrate(Substrate([20.25], [1,], 'Silicone-Wafer'))
+DBR.add(Ambient(1, 1, 'Air'))
+DBR.add(Substrate(20.25, 1, 'Silicone-Wafer'))
 
 for i in range(8):
-    DBR.add_layer([ThinLayer(123e-9, [4.0779], [1], name=f'Si3N4 {i}'), ThinLayer(151e-9, [2.1025], [1], name=f'SiO2 {i}') ])
+    DBR.add([ThinLayer(123e-9, 4.0779, 1, name=f'Si3N4 {i}'), ThinLayer(151e-9, 2.1025, 1, name=f'SiO2 {i}') ])
 
 DBR.Radiation(885e-9)
 DBR.render()
+print(f"Reflectance:{DBR.reflectance}% Transmittance:{DBR.transmittance}%")
